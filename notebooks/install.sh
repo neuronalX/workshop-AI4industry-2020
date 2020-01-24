75f7440418558b09cc6dadf577137cee97adc247
HINAUT Xavier <xavier.hinaut@inria.fr>
2020-01-24 10:11:34 +0100

    added bigger word embedding model with 1 million unique vectors

--- notebooks/install.sh
+++ notebooks/install.sh
@@ -2,3 +2,4 @@ pip install -U spacy
 python -m spacy download en_core_web_sm # only word embedding based on parser, tagger and NER (i.e. not word2vec like)
 python -m spacy download fr_core_news_sm
 python -m spacy download en_core_web_md # for word embedding with pre-trained models
+python -m spacy download en_vectors_web_lg # word embedding including 1 million unique vectors
