3bb12809dbcd0d0dae78790a5836a56d0e5107c4
HINAUT Xavier <xavier.hinaut@inria.fr>
2020-01-24 09:17:06 +0100

    added new corpus with pretrained word embeddings

--- notebooks/install.sh
+++ notebooks/install.sh
@@ -1,3 +1,4 @@
 pip install -U spacy
-python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_sm # only word embedding based on parser, tagger and NER (i.e. not word2vec like)
 python -m spacy download fr_core_news_sm
+python -m spacy download en_core_web_md # for word embedding with pre-trained models
